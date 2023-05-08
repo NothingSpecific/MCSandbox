5f3979527144affb97725d26eef4e6010dd00c31
BuildTools <cumpootuhr@gmail.com>
2023-05-08 14:57:16 -0600

    First version

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # MCSandbox
-A Minecraft sandbox script written to work as a Prism Launcher wrapper command
+A Minecraft sandbox script written to work as a Prism Launcher wrapper command.
+
+Usage is simple. Just add this script as a Prism Launcher wrapper command either in the launcher's global settings, or in the settings for the specific instance you plan to use this with.
+
+Optionally you can install `mcsandbox.sh` into your system's `$PATH`.
+
+This script should work by default without adding any options to the command-line. Note that it does not do any X11 sandboxing, so X11-based attacks are still possible unless you use Wayland. I'm comfortable with this since X11 keylogging and screen grabbing are purely passive attacks. As long as you don't type anything sensitive while Minecraft is open, you shouldn't have to worry about these. Note that some attacks may try to fake keystrokes to interact with the system, but these attacks are trivial to spot by watching the system (unless you leave the system unattended you're going to know if a terminal randomly opens up).
+
+To improve system compatibility and make my life easier, this script gives full read-only access to the filesystem, except for `/home`. This script trusts that your system's filesystem permissions are properly configured to deny read access to anything too sensitive.
---
+++ mcsandbox.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+XPRA_DISPLAY=0
+
+# Xpra commented out due to issues with performance and mouse handling
+# Perhaps using a different X server might solve the issues, but so far I'm too lazy and
+# I think anything more than screen/clipboard grabbing would be detected by the user
+# allowing the opportunity to interrupt the attack
+
+#XPRA_DISPLAY="$RANDOM"
+#xpra start :"$XPRA_DISPLAY"
+#while :; do
+#	[[ -e /tmp/.X11-unix/X"$XPRA_DISPLAY" ]] && break
+#	echo "Waiting for xpra display $XPRA_DISPLAY"
+#	sleep 1
+#done
+#xpra attach :"$XPRA_DISPLAY" &
+
+echo "Command to be sandboxed:"
+echo "$@"
+
+declare -a EXTRA_OPTS=()
+declare -a COMMAND=()
+
+for i in "$@"; do
+	if [[ "$i" == "--full-launcher-access" ]]; then
+		EXTRA_OPTS+=(--bind ~/.local/share/PrismLauncher/ ~/.local/share/PrismLauncher/)
+	elif [[ "$i" == "--libs" ]]; then
+		EXTRA_OPTS+=(--bind ~/.local/share/PrismLauncher/libraries/ ~/.local/share/PrismLauncher/libraries/)
+	else
+		COMMAND+=("$i")
+	fi
+done
+
+# Set environment variables
+export WURST_OPENAI_KEY='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'
+
+# This command was written for X11 and has not been tested on Wayland
+CMD=(bwrap --ro-bind / / --tmpfs /var/tmp --tmpfs /home/ --tmpfs /tmp \
+	--bind /tmp/.X11-unix/X"$XPRA_DISPLAY" /tmp/.X11-unix/X"$XPRA_DISPLAY" --setenv DISPLAY :"$XPRA_DISPLAY" \
+	--ro-bind ~/.Xauthority ~/.Xauthority \
+	--unshare-all --share-net --new-session --cap-drop ALL --die-with-parent \
+	--dev-bind /dev/ /dev/ --proc /proc --ro-bind ~/.local/share/PrismLauncher/ ~/.local/share/PrismLauncher/ --bind "$INST_DIR" "$INST_DIR" \
+	--bind-try "$XDG_RUNTIME_DIR/pipewire-0" "$XDG_RUNTIME_DIR/pipewire-0" \
+	--bind "$XDG_RUNTIME_DIR/pulse" "$XDG_RUNTIME_DIR/pulse" "${EXTRA_OPTS[@]}" \
+	"${COMMAND[@]}")
+
+echo "Command to start sandbox:"
+echo "${CMD[@]}"
+echo "----------------------------"
+
+# Start sandbox
+"${CMD[@]}"
+
+# Cleanup Xpra server
+#xpra stop :"$XPRA_DISPLAY"
